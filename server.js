77f46ee4f65ac835635d44844552406759d7acac
Dhiraj Kumar <dhirajkumar464748@gmail.com>
2026-03-18 20:36:10 +0530

    fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
-
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const Razorpay = require('razorpay');
